d1b1521c3d72d39e2639a6fdcda6e86b16531a60
Netovanniy <netovanniy@gmail.com>
2026-08-28 22:26:00 +0700

    footer: nudge COMPANY column another 10px left

--- _build/gen_pages.py
+++ _build/gen_pages.py
@@ -25,7 +25,7 @@ def head(title, desc):
 <title>{title}</title>
 <meta name="description" content="{desc}">
 <link rel="icon" href="assets/img/logo-mark-white.png">
-<link rel="stylesheet" href="assets/css/style.css?v=28">
+<link rel="stylesheet" href="assets/css/style.css?v=29">
 </head>
 <body>
 <a class="skip-link" href="#main">Skip to content</a>
